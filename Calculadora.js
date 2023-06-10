40491aab168de3cc161c184569a915c3522d99af
Nana Villarroel <icaros851005@gmail.com>
2023-06-10 16:41:38 -0300

    done

---
+++ Calculadora.js
@@ -0,0 +1,14 @@
+class Calculadora {
+    suma(num1, num2){
+        return num1 + num2;
+    }
+    resta(num1,num2){
+        return num1 - num2;
+    }
+    multiplica(num1, num2){
+        return num1 * num2;
+    }
+    divide(num1, num2){
+        return num1 / num2;
+    }
+}
